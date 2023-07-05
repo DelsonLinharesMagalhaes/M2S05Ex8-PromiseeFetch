b2312260d3720823d08b764445488bd3165eaac8
Delson Magalhaes <dl_magalhaes@hotmail.com>
2023-07-05 13:22:25 -0300

    third commit

--- config.js
+++ config.js
@@ -3,29 +3,27 @@ const promise = new Promise((resolve, reject) => {
       resolve('Algum tempo depois...');
     }, 3000)
 });
+
+
 promise.then(response => {
     console.log(response);
 });
   console.log('Imediato!');
 
 
-const buscarPokemon = fetch('https://www.pokemon.com/br/');
-buscarPokemon
-.then(res => {
-    return res.json();
-})
-.then(res => {
-    console.log(res);
-});
-
-
-const pokemons = [];
-const neededPokemons = [ 1, 2, 3 ];
-for (let i = 0; i < neededPokemons.length; i++) {
-  pokemons.push(
-    fetch(`https://pokeapi.co/api/v2/pokemon/${ neededPokemons[i] }/`)
-      .then(res => res.json())
-  );
+function getUser(id){
+  return fetch(`https://reqres.in/api/users?id=${id}`)
+  .then((data) => data.json())
+  .catch((err)=> console.log('aconteceu um erro' + err));
 }
-await Promise.all(pokemons);
-console.log(pokemons);
+
+async function showUserName(id){
+  try{
+    const user = await getUser(id)
+    console.log(`o numero do usuario é: ${user.data.first_name}`)
+  } catch (error){
+    console.log(`Erro: ${error}`);
+  }
+}
+
+showUserName(1);
